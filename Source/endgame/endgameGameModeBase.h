60101e5ce9c2e1deb78a0606687c77511ec08bb8
Daniel Contreras Mejia <daniel.c82@gmail.com>
2020-06-29 19:19:09 -0400

    first commit
    Begins the project. Adds all the minimum files for the unreal engine project.

---
+++ Source/endgame/endgameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "endgameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ENDGAME_API AendgameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
